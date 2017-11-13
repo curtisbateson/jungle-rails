f85cc4c344891b94fdba10cceccd5ce7915bcb18
Curtis Bateson <curtislbateson@gmail.com>
2017-11-13 07:31:03 +0000

    Completed main page review display

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,20 @@ class Product < ActiveRecord::Base
   validates :quantity, presence: true
   validates :category, presence: true
 
+  def review_total(id)
+    reviews = Review.all.where("product_id = #{id}")
+    if reviews.length > 0
+      total = 0
+      reviews.each do |review|
+        total += review.rating
+      end
+      total /= reviews.length
+      if total > 0
+        "Average rating: ".concat(total.to_s)
+      end
+    else
+      "No reviews"
+    end
+  end
+
 end
